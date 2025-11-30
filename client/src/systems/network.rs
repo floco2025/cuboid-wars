983ab9753c13c274b10f1e50fed77b0c97df58fb
Marc Laukien <floco2025@users.noreply.github.com>
2025-11-30 08:10:43 -0500

    refactor: remove unused RoundTripTime parameter from message handling functions

--- client/src/systems/network.rs
+++ client/src/systems/network.rs
@@ -119,7 +119,6 @@ fn process_message_logged_in(
             materials,
             images,
             players,
-            rtt,
             player_query,
             camera_query,
             my_player_id,
@@ -208,7 +207,6 @@ fn handle_update_message(
     materials: &mut ResMut<Assets<StandardMaterial>>,
     images: &mut ResMut<Assets<Image>>,
     players: &mut ResMut<PlayerMap>,
-    _rtt: &ResMut<RoundTripTime>,
     player_query: &Query<(&Position, &Velocity), With<PlayerId>>,
     camera_query: &Query<Entity, With<Camera3d>>,
     my_player_id: PlayerId,
@@ -277,18 +275,12 @@ fn handle_update_message(
     for (id, server_player) in &msg.players {
         if let Some(client_player) = players.0.get_mut(id) {
             if let Ok((client_pos, client_vel)) = player_query.get(client_player.entity) {
-                //let half_rtt_secs = rtt.rtt.as_secs_f32() / 2.0;
                 let server_vel = server_player.speed.to_velocity();
 
                 commands.entity(client_player.entity).insert(ServerSnapshot {
                     client_pos: *client_pos,
                     client_vel: *client_vel,
                     server_pos: server_player.pos,
-                    // server_pos: Position {
-                    //     x: server_vel.x.mul_add(half_rtt_secs, server_player.pos.x),
-                    //     y: server_player.pos.y,
-                    //     z: server_vel.z.mul_add(half_rtt_secs, server_player.pos.z),
-                    // },
                     server_vel,
                     received_at: now,
                     timer: 0.0,
